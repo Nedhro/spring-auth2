b1336bb7631f869749f277ecbaca6e2b122f4185
nedhro <nedhro786>
2021-03-08 12:24:41 +0600

    Added dependencies

---
+++ src/main/java/com/prodev/auth2demo/Auth2demoApplication.java
@@ -0,0 +1,13 @@
+package com.prodev.auth2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Auth2demoApplication.class, args);
+    }
+
+}
